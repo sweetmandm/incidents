b4663b1e8e3fd7502b87f4ba552c7bb417fd0367
David Sweetman <david@davidsweetman.com>
2018-09-02 16:29:26 -0400

    Remove api error response view dependency

--- server/src/app.js
+++ server/src/app.js
@@ -16,11 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -29,7 +24,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
 module.exports = app;
