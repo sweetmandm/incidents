52450858ed42aae7a7617b4701f5a9bf04209aec
David <david@davidsweetman.com>
2018-10-23 15:09:45 -0400

    Add open/close functionality on list

--- client/src/components/IncidentList.tsx
+++ client/src/components/IncidentList.tsx
@@ -12,21 +12,27 @@ interface IState {
   incidents: IIncident[];
   onClick: (incident: IIncident) => void;
   selected: IIncident | null;
+  listOpen: boolean | undefined;
 }
 
 class IncidentList extends React.Component<IProps, IState> {
+  public el: HTMLElement | null = null;
+
   public constructor(props: IProps) {
     super(props);
     this.state = {
       incidents: props.incidents,
+      listOpen: false,
       onClick: props.onClick,
-      selected: props.selected
+      selected: props.selected,
     };
   }
 
   public componentWillReceiveProps(nextProps: IProps) {
+    const isNewSelection = this.madeNewSelection(nextProps);
     this.setState({
       incidents: nextProps.incidents,
+      listOpen: isNewSelection ? false : this.state.listOpen,
       onClick: nextProps.onClick,
       selected: nextProps.selected
     });
@@ -35,10 +41,26 @@ class IncidentList extends React.Component<IProps, IState> {
   public render() {
     return (
       <div>
-        <div><button>List</button></div>
-        <div className='incident-list sm-closed'>
+        <div>
+          <button
+            className='list-toggle'
+            onClick={this.toggleList}
+          >
+            List
+          </button>
+        </div>
+        <div
+          className={this.buildClassName()}
+        >
           <div className='title'>
             <h2>🔥 Incidents</h2>
+            <button
+              className='close-btn'
+              onClick={this.toggleList}
+              aria-label='Close List'
+            >
+              x
+            </button>
           </div>
           <div className='incidents'>
             {this.state.incidents.map((i) => {
@@ -58,6 +80,30 @@ class IncidentList extends React.Component<IProps, IState> {
   protected isSelected(incident: IIncident) {
     if (this.state.selected === null) { return false; }
     return this.state.selected._id === incident._id;
+  }
+
+  protected madeNewSelection(newProps: IProps) {
+    return (newProps.selected == null ||
+            this.state.selected == null ||
+            newProps.selected._id !== this.state.selected._id);
+  }
+
+  protected toggleList = () => {
+    this.setState({
+      ...this.state,
+      listOpen: !this.state.listOpen
+    });
+  }
+
+  protected closeList = () => {
+    this.setState({
+      ...this.state,
+      listOpen: false
+    });
+  }
+
+  protected buildClassName(): string {
+    return `incident-list ${this.state.listOpen ? 'sm-open' : 'sm-closed'}`;
   }
 }
 
